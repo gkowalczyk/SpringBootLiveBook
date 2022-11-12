2ff52ac9099e654ae551898c3ad70a3195216a72
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2022-11-12 14:49:16 +0100

    commit

---
+++ src/main/java/com/example/springbootlivebook/SpringBootLiveBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootlivebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiveBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLiveBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootlivebook/SpringBootLiveBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlivebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiveBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
